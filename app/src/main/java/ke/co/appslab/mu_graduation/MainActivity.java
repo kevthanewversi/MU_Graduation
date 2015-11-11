a9d1e414e337dbbba283c3fb64db6ca5a957f00a
kevthanewversi <murimiclerk@gmail.com>
2015-11-11 20:56:25 +0300

    adding of viewpagerindicator started

--- app/src/main/java/ke/co/appslab/mu_graduation/MainActivity.java
+++ app/src/main/java/ke/co/appslab/mu_graduation/MainActivity.java
@@ -20,7 +20,7 @@ import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks{
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
