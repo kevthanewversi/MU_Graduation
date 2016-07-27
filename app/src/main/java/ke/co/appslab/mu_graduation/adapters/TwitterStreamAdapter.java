43d04f8d9b2a88de0f6d6fb68d4477c7cb8ce133
kevthanewversi <murimiclerk@gmail.com>
2016-07-27 14:19:18 +0300

    *added picasso to gradle        to load user's twitter avatar

--- app/src/main/java/ke/co/appslab/mu_graduation/adapters/TwitterStreamAdapter.java
+++ app/src/main/java/ke/co/appslab/mu_graduation/adapters/TwitterStreamAdapter.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 import ke.co.appslab.mu_graduation.R;
